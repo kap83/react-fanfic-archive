a11a10f0068443fc5c990bcde9b07d3fa7a59be3
Karen Polanco <polanco.k@gmail.com>
2023-03-10 20:44:47 -0500

    commit message

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,39 +1,30 @@
 //will include titles, authors. one of the users should be u. cause you want to be able to star another favorite author
 
-import React from 'react'
+import React, {useState} from 'react'
 import '../index.css'
 
 export default function NavBar() {
+
+  const [user, setUser] = useState(users)
+  
+  console.log(user)
+
   return (
     <nav className='navBar'>
       <form className='form'>
-      <label>SEARCH:</label>
-      <select>
-        <option><a href='/Author'>AUTHOR</a></option>
-        <option><a href='/Fandom'>FANDOM</a></option>
-      </select>
+        <label>SEARCH:</label>
+        <select>
+          <option><a href='/Author'>AUTHOR</a></option>
+          <option><a href='/Fandom'>FANDOM</a></option>
+        </select>
+      </form>
       <span className='btn'>
+        <a href='/username'>username</a>
         <button>Profile</button>
         <button>Post</button>
       </span>
-      </form>
     </nav>
 
   
   )
 }
-
-
-// username
-//         userimage 
-    //     <button style={{float: "right"}}>Profile</button>
-    //     <button style={{float: "right"}}>Post</button>
-    //  <form>
-    //    <label>
-    //      Search: 
-    //      <select>
-    //        <option value="Fandom">Fandom</option>
-    //        <option value="Author">Author</option>
-    //      </select> 
-    //    </label>
-    //  </form>
