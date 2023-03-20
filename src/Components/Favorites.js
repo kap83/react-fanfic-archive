295fe2a3b5c5e3bfd2de795289c6e8ca9065f9d3
Karen Polanco <polanco.k@gmail.com>
2023-03-20 15:10:11 -0400

    commit message

--- src/Components/Favorites.js
+++ src/Components/Favorites.js
@@ -4,16 +4,17 @@ import '../index.css'
 
 
 export default function Favorites({userData}) {
+
   return (
     <>
       <h1 style={{display: "flex", marginLeft: "45%"}}>FAVORITE AUTHORS</h1>
       {userData.map((user) => user.favorite === "yes" ?  
         <div key={user.id} className= 'card-container' style={{marginTop:"3%", marginLeft: "25%", marginRight: "-20%"}}>
         <div className='image-container'><img src={user.icon} alt="icon" /></div>
-          <h2>{user.username} ❤️</h2>
-            <h3 style={{marginBottom: "none", padding: "0px"}}>Fandoms:</h3>
-            {user.fandoms.map((fandom, index) => <ul key={index} style={{borderStyle: "solid", justifyContent: "space-between", display: "inline-block", padding: "0%",  gap: "10rem", listStyleType: "none", color: "red"}}><li>{fandom}</li></ul>)}
-            <h3>titles:</h3> {user.titles.map((title, index) => <div key={index}><p>{title}</p></div>)}
+          <h2 style={{borderStyle: "solid", color: "blue"}}>{user.username} ❤️</h2>
+            <h3 style={{borderStyle: "solid", marginBottom: "1%", padding: "0px"}}>Fandoms:</h3>
+            {user.fandoms.map((fandom) => <ul key={crypto.randomUUID()} style={{borderStyle: "solid", display: "inline-block", marginTop: "0" , marginBottom:"1%", padding: "0%", listStyleType: "none", color: "red"}}><li>{fandom}</li></ul>)}
+            <h3 style={{borderStyle: "solid", color: "green"}}>titles:</h3> {user.titles.map((title) => <div key={crypto.randomUUID()}><p>{title}</p></div>)}
         </div>: null)}
     </>
   )
