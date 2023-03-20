88438476febb3513a2389baa582d86fedae55074
Karen Polanco <polanco.k@gmail.com>
2023-03-20 15:26:08 -0400

    commit message

--- src/Components/Favorites.js
+++ src/Components/Favorites.js
@@ -14,7 +14,7 @@ export default function Favorites({userData}) {
           <h2 style={{borderStyle: "solid", color: "blue"}}>{user.username} ❤️</h2>
             <h3 style={{borderStyle: "solid", marginBottom: "1%", padding: "0px"}}>Fandoms:</h3>
             {user.fandoms.map((fandom) => <ul key={crypto.randomUUID()} style={{borderStyle: "solid", display: "inline-block", marginTop: "0" , marginBottom:"1%", padding: "0%", listStyleType: "none", color: "red"}}><li>{fandom}</li></ul>)}
-            <h3 style={{borderStyle: "solid", color: "green"}}>titles:</h3> {user.titles.map((title) => <div key={crypto.randomUUID()}><p>{title}</p></div>)}
+            <h3 style={{borderStyle: "solid", color: "green", marginTop: "0"}}>titles:</h3> {user.titles.map((title) => <ul key={crypto.randomUUID()}><li style={{listStyleType: "none", borderStyle: "solid", color: "pink" }}>{title}</li></ul>)}
         </div>: null)}
     </>
   )
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -10,8 +10,14 @@ export default function Home({userData}) {
   return (
     <div>
       <h1 style={{display: "flex", marginLeft: "45%"}}>HOME PAGE</h1>
-      {/* {userData.map((user) => console.log(user))} */}
-
+       {userData.map((user) => user.username === "dragonwitch" ? <div key={user.id} className= 'card-container' style={{marginTop:"3%", marginLeft: "25%", marginRight: "-20%"}}>
+         <div className='image-container'><img src={user.icon} alt="icon" /></div>
+         <h2 style={{borderStyle: "solid", color: "blue"}}>{user.username} {}</h2>
+            <h3 style={{borderStyle: "solid", marginBottom: "1%", padding: "0px"}}>Fandoms:</h3>
+            {user.fandoms.map((fandom) => <ul key={crypto.randomUUID()} style={{borderStyle: "solid", display: "inline-block", marginTop: "0" , marginBottom:"1%", padding: "0%", listStyleType: "none", color: "red"}}><li>{fandom}</li></ul>)}
+            <h3 style={{borderStyle: "solid", color: "green", marginTop: "0"}}>titles:</h3> {user.titles.map((title) => <ul key={crypto.randomUUID()}><li style={{listStyleType: "none", borderStyle: "solid", color: "pink" }}>{title}</li></ul>)}
+         </div> : null
+       )}
     </div>
   )
 }
